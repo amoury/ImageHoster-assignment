0a975add5caff1c7f8d0d3f31eea7009ed45fe4f
Amoury <ansar.memon@hotmail.com>
2019-04-07 06:57:53 +0400

    :rewind: Reverted back the comment textarea field name to 'comment' and made adjustments to comment controller

--- src/main/java/ImageHoster/controller/CommentController.java
+++ src/main/java/ImageHoster/controller/CommentController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
@@ -31,9 +32,10 @@ public class CommentController {
     // This method is called when the user submits the comment form.
     // It add the user and image fields and then sends it off the service layer for persistence.
     @RequestMapping(value = "/image/{imageId}/{imageTitle}/comments", method = RequestMethod.POST)
-    public String createComment(@PathVariable("imageTitle") String title, @PathVariable("imageId") Integer id, Comment newComment, HttpSession session, RedirectAttributes redirectAttrs) throws IOException {
+    public String createComment(@PathVariable("imageTitle") String title, @PathVariable("imageId") Integer id, @RequestParam("comment") String comment, Comment newComment, HttpSession session, RedirectAttributes redirectAttrs) throws IOException {
         User user = (User) session.getAttribute("loggeduser");
         newComment.setUser(user);
+        newComment.setText(comment);
         newComment.setCreatedDate(LocalDate.now());
         newComment.setImage(imageService.getImage(id));
 
